da1ae391b8912e64367bbacdc9d71fdddf6499ee
sersoong <sersoong@gmail.com>
2019-07-14 07:06:35 +0800

    更新mysql数据存储目录

--- docker_mysql.sh
+++ docker_mysql.sh
@@ -2,6 +2,6 @@ docker run -d \
     --name sersoong-mysql \
     --network sersoongnet \
     -e MYSQL_ROOT_PASSWORD="123456" \
-    -v /tmp/data:/var/lib/mysql \
+    -v /var/mysqldata:/var/lib/mysql \
     -p 3306:3306 \
     mysql:5.7
