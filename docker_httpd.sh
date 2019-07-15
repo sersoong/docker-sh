97a395bc68f7164c44406973190b69febf2ca8f0
sersoong <sersoong@gmail.com>
2019-07-15 14:00:49 +0800

    Update docker_httpd.sh

--- docker_httpd.sh
+++ docker_httpd.sh
@@ -1,1 +1,1 @@
-docker run -d --network sersoongnet --name sersoong-httpd-pdo -v /home/sersoong/code/www:/var/www/localhost/htdocs -p 8000:80 alpine:httpd-pdo docker-run.sh
+docker run -d --network sersoongnet --name sersoong-httpd-json -v /home/sersoong/code/www:/var/www/localhost/htdocs -p 8000:80 alpine:httpd4-json docker-run.sh
