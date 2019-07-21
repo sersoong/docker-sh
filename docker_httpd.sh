404e41c13a87e55eb6cbe7de0da3a8fc82b61606
sersoong <sersoong@gmail.com>
2019-07-21 10:33:47 +0800

    update

--- docker_httpd.sh
+++ docker_httpd.sh
@@ -1,6 +1,6 @@
 docker run -d \
     --network sersoongnet \
-    --name sersoong-httpd-session \
+    --name sersoong-httpd-mbstring \
     -v /home/sersoong/code/www:/var/www/localhost/htdocs \
-    -p 8000:80 alpine:httpd5-session \
+    -p 8000:80 alpine:httpd6-mbstring \
     docker-run.sh
