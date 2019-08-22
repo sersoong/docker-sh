993c1540f1615a8e00d072f1d03839ae11c4d2b8
sersoong <sersoong@gmail.com>
2019-08-22 16:04:25 +0800

    add windows bat

---
+++ docker_httpd.bat
@@ -0,0 +1,1 @@
+docker run -d --network sersoongnet --name sersoong-httpd-xdebug -v F:/code/sanmiaogw-backend:/var/www/localhost/htdocs -p 8001:80 registry.cn-shenzhen.aliyuncs.com/sersoong/alpine:httpd7-xdebug docker-run.sh
